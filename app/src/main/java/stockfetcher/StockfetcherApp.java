bfbd0bffcb52dc36a8aa196250f5104f7154f7f0
Caleb Cassady <caleb.cassady@hotmail.com>
2021-10-31 18:19:23 -0500

    added JavaFX dependency

---
+++ app/src/main/java/stockfetcher/StockfetcherApp.java
@@ -0,0 +1,17 @@
+package stockfetcher;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class StockfetcherApp extends Application {
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.show();
+	}
+
+}
